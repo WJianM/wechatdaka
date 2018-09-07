f6f0122492507a36950e70a94a42310f3a4d40a8
WJM <wodezhanghaozhijia@gmail.com>
2018-09-07 12:22:21 +0800

    项目首次初始化导入

--- src/com/daka/utils/DateUtils.java
+++ src/com/daka/utils/DateUtils.java
@@ -9,7 +9,8 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.Locale;
-import org.apache.commons.lang.StringUtils;
+//import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 public class DateUtils {
